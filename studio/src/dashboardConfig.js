43950b988f994ad7cae6342668c918dda35bbec7
holly-searchengineunity <holly@searchengineunity.com>
2020-12-15 14:53:17 -0500

    deploy button added to studio

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'reports.searchengineunity.com',
+            apiId: 'df70d8db-33d3-4f03-859d-e8c3fb92fb99',
+            buildHookId: '5f5fa91b2de742303dd95c45',
+            name: 'boring-kirch-967873',
+          },
+        ],
+      },
+    },
+  ],
+};
